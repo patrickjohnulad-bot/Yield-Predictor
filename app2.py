011bf89566209febd8baba694cda7446983145db
Patrick John <patrickjohn.ulad@gmail.com>
2026-03-23 21:31:42 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -378,5 +378,4 @@ st.markdown("---")
 st.markdown("""
 *Powered by ARIMAX-SVR Hybrid Model | Data: HarvestStat Africa | Climate: NASA POWER API*
 """)
-st.markdown("---")
-st.markdown("👨‍💻 Created by Patrick John | Thesis Project")
+st.markdown("Created by Patrick John | Thesis Project")
